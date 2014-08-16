f24e2b5c8df98b5f71bb0c318780118b26e8ef07
shen.xianpeng <shenxianpeng1991@gmail.com>
2014-08-16 23:29:56 +0800

    uwsgi django nginx ALLOWED_HOST  DEBUG *remember* to rm static suffix because nginx will auto add 'static' suffic

--- design/urls.py
+++ design/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from .views import IndexView
 from notifications.views import NotificationView
 
@@ -13,3 +14,8 @@ urlpatterns = patterns('',
     url(r'^notifications/', NotificationView.as_view(), name='design-notification'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+   urlpatterns += patterns('', url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+           url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}), )
